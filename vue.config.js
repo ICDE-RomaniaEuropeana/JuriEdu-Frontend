ac500470d98fb722663386d96c22968568166689
Aurel Canciu <aurelcanciu@gmail.com>
2019-11-12 17:17:14 +0200

    Set default dev port

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,7 @@ function resolveSrc(_path) {
 // vue.config.js
 module.exports = {
   lintOnSave: true,
+  devServer: { port: 4000 },
   configureWebpack: {
     // Set up all the aliases we use in our app.
     resolve: {
